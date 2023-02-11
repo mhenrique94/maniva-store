71ff8544d43100395a63bb2f4f80f203c9d9807b
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2023-02-11 00:48:23 -0300

    tried to build frontend's container

--- store_frontend/vue.config.js
+++ store_frontend/vue.config.js
@@ -2,14 +2,14 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "http://localhost:8080",
-  outputDir: "../static/dist",
-  indexPath: "../../templates/index.html",
+  // outputDir: "../static/dist",
+  // indexPath: "../../templates/index.html",
 
-  configureWebpack: {
-    devServer: {
-      devMiddleware: {
-        writeToDisk: true,
-      },
-    },
-  },
+  // configureWebpack: {
+  //   devServer: {
+  //     devMiddleware: {
+  //       writeToDisk: true,
+  //     },
+  //   },
+  // },
 });
