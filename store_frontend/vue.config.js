e783ca3f0d6b7db3874b1739ff6408645e41815e
mhenrique94 <mhenrique94@outlook.com>
2024-01-06 00:17:05 -0300

    Revert "remove fly.io front"

--- store_frontend/vue.config.js
+++ store_frontend/vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === 'production'
-  // ? 'https://maniva-store-front.fly.dev'
-  // : 'http://localhost:8080',
-  outputDir: "./dist",
+  publicPath: process.env.NODE_ENV === 'production'
+  ? 'https://maniva-store-front.fly.dev'
+  : 'http://localhost:8080',
+  // outputDir: "../static/dist",
   // indexPath: "../../templates/index.html",
 
   // configureWebpack: {
